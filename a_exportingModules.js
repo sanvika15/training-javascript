3c6e84ec0704f62388607c1abe35d915b5cb2788
Sanvika Dakhale <137372400+sanvika15@users.noreply.github.com>
2025-06-27 11:17:55 +0530

    Add files via upload

---
+++ a_exportingModules.js
@@ -0,0 +1,28 @@
+ export var color = "red";
+ export function setName(newName){
+    color = newName;
+ }
+ let name  = "Sanvika"
+
+ export const magicNumber =7;
+
+ export function sum(num1 , num2){
+    return num1 + num2;
+ }
+
+ export class Rectanngle {
+    constructor(length, width){
+        this.length = length;
+        this.width = width;
+    }
+ }
+
+ function substract(num1,num2){
+    return num1 - num2;
+ }
+
+ function multiply(num1,num2){
+    return num1 * num2;
+ }
+
+ export { substract , multiply }
